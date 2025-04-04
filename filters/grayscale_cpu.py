46ac7a6c60f57d6354c3e83cb02aee0a8f88d6bc
Anthony Cano <132002947+AnthonyCano@users.noreply.github.com>
2025-04-04 10:46:38 -0700

    adding the logic block to run in the command line

--- filters/grayscale_cpu.py
+++ filters/grayscale_cpu.py
@@ -12,3 +12,19 @@ def to_grayscale_cpu(image):
     '''
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+
+# If block for the script to be run in the command line
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", required=True)
+    args = parser.parse_args()
+
+    # This reads the image and returns it as a OpenCV matrix
+    image = cv2.imread(args.input)
+    gray = to_grayscale_cpu(image)
+
+    cv2.imshow("Grayscale (CPU)", gray)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
