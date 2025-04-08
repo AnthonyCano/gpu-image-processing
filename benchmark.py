fe9203a460ea28eed4a933e8cefd16d4c92791f8
Anthony Cano <132002947+AnthonyCano@users.noreply.github.com>
2025-04-07 22:58:26 -0700

    adding the cpu testing

--- benchmark.py
+++ benchmark.py
@@ -4,3 +4,10 @@ from filters.grayscale_cpu import to_grayscale_cpu
 from filters.grayscale_gpu_numba import to_grayscale_gpu
 
 image = cv2.imread("images/test.jpg")
+
+# CPU 
+start = time.perf_counter()
+cpu_result = to_grayscale_cpu(image)
+end = time.perf_counter()
+
+
