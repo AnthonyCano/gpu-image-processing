309a57bac9e3252b61949eecd6848d9159a9e366
Anthony Cano <132002947+AnthonyCano@users.noreply.github.com>
2025-04-07 23:01:20 -0700

    Fixed the import for the gpu function, added print for cpu, and gpu timing logic

--- benchmark.py
+++ benchmark.py
@@ -1,7 +1,7 @@
 import time
 import cv2
 from filters.grayscale_cpu import to_grayscale_cpu
-from filters.grayscale_gpu_numba import to_grayscale_gpu
+from filters.grayscale_gpu_numba import apply_gpu_filter
 
 image = cv2.imread("images/test.jpg")
 
@@ -9,5 +9,10 @@ image = cv2.imread("images/test.jpg")
 start = time.perf_counter()
 cpu_result = to_grayscale_cpu(image)
 end = time.perf_counter()
+print(f"CPU Time: {(end -start) * 1000:.2} ms")
 
-
+# GPU
+start = time.perf_counter()
+gpu_result = apply_gpu_filter(image)
+end = time.perf_counter()
+print(f"GPU Time: {(end - start) * 1000:.2f} ms")
